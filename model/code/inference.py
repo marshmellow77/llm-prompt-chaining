91e7f3e811993db8ca59d609ee069bca0ec4c175
Your Name <you@example.com>
2023-02-05 13:24:21 +0000

    initial commit

--- model/code/inference.py
+++ model/code/inference.py
@@ -1,20 +1,12 @@
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 import torch
 import os
-import subprocess
 
 
 def model_fn(model_dir):
-    output = subprocess.check_output(["df", "-h"])
-    print(output.decode('UTF-8'))
-    
     files = os.listdir(model_dir)
-    print(f"Model directory: {model_dir}")
-    print(f"Files: {files}")
     has_bin_files = any(file.endswith('.bin') for file in files)
-    print(f"Has .bin files: {has_bin_files}")
-    
-    model_name = ""
+    model_name = ""    
     
     if has_bin_files:
         model_name = model_dir
@@ -23,27 +15,20 @@ def model_fn(model_dir):
             with open(f"{model_dir}/model_name.txt", 'r') as file:
                 model_name = file.read()
         except FileNotFoundError:
-            print("File does not exist.")
-        
-    print(f"Model name: {model_name}")
-    print("Model loadining start")
+            print("Model name file does not exist.")
+            exit(1)
 
     model_8bit = AutoModelForSeq2SeqLM.from_pretrained(model_name, device_map="auto", load_in_8bit=True, cache_dir="/tmp/model_cache/")
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     
-    output = subprocess.check_output(["df", "-h"])
-    print(output.decode('UTF-8'))
-    
-    print("Model loadining end")
-
     return model_8bit, tokenizer
 
 
 def predict_fn(data, model_and_tokenizer):
     model, tokenizer = model_and_tokenizer
     text = data.pop("inputs", data)
-    print(f"Prompt: {text}")
+
     encoded_input = tokenizer(text, return_tensors='pt')
-    print(f"Data: {data}")
     outputs = model.generate(input_ids=encoded_input['input_ids'].cuda(), **data)
+    
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
